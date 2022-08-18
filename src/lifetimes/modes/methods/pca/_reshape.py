eba8a7c3a187da71d469e5f3dadb991efeadde58
Fabian Emmerich <fabian.emmerich@4-cast.de>
2022-08-18 12:31:53 +0200

    Refactor and add docstring

--- src/lifetimes/modes/methods/pca/_reshape.py
+++ src/lifetimes/modes/methods/pca/_reshape.py
@@ -34,39 +34,68 @@ class Reshaper:
         if rename_dim_0:
             data = _rename_1d_data_array_dimension(data)
 
-        variables = self._vars_to_xarray(
+        reshaped_entries = self._reshape_entries(
             data, includes_time_dimension=includes_time_dimension
         )
         return xr.Dataset(
-            data_vars={str(da.name): da for da in variables},
+            data_vars={str(da.name): da for da in reshaped_entries},
         )
 
-    def _vars_to_xarray(
+    def _reshape_entries(
         self, data: xr.DataArray, includes_time_dimension: bool
     ) -> t.Iterator[xr.DataArray]:
+        """Reshape the entries of a PCA.
+
+        Notes
+        -----
+        Before performing a PCA, the gridded `(m x n)` data of each variable in
+        a dataset is flattened and concatenated for each time step. For N
+        variables and T time steps, this results in a `(T x m * n * N)` matrix.
+        Hence, to get the PC of an individual variable `i of N` in the shape of
+        the `(m x n)` grid, this can be undone by taking the
+        `(i - 1) * (m * n)`th until the `i * (m * n)`th entry of a PC and
+        reshaping it into the original grid shape `(m x n)`.
+
+        """
         for name, slice_ in self._dimensions.to_variable_name_and_slices():
             values = data.sel({pca.PC_VALUES_DIM: slice_})
             if includes_time_dimension:
-                reshaped = values.data.reshape(self._dimensions.to_tuple())
-                # The PCs are flipped along axis 1.
-                yield xr.DataArray(
-                    np.flip(reshaped, axis=1),
-                    dims=[
-                        pca.PC_DIM,
-                        *self._dimensions.spatial_dimension_names,
-                    ],
+                yield self._reshape_spatio_temporal_entries(
+                    data=values,
                     name=name,
                 )
             else:
-                reshaped = values.data.reshape(
-                    self._dimensions.to_tuple(include_time_dim=False)
-                )
-                # The PCs are flipped along axis 0.
-                yield xr.DataArray(
-                    np.flip(reshaped, axis=0),
-                    dims=self._dimensions.spatial_dimension_names,
+                yield self._reshape_spatial_entries(
+                    data=values,
                     name=name,
                 )
+
+    def _reshape_spatio_temporal_entries(
+        self, data: xr.DataArray, name: str
+    ) -> xr.DataArray:
+        reshaped = data.data.reshape(self._dimensions.to_tuple())
+        # The PCs are flipped along axis 1.
+        return xr.DataArray(
+            np.flip(reshaped, axis=1),
+            dims=[
+                pca.PC_DIM,
+                *self._dimensions.spatial_dimension_names,
+            ],
+            name=name,
+        )
+
+    def _reshape_spatial_entries(
+        self, data: xr.DataArray, name: str
+    ) -> xr.DataArray:
+        reshaped = data.data.reshape(
+            self._dimensions.to_tuple(include_time_dim=False)
+        )
+        # The PCs are flipped along axis 0.
+        return xr.DataArray(
+            np.flip(reshaped, axis=0),
+            dims=self._dimensions.spatial_dimension_names,
+            name=name,
+        )
 
 
 def _rename_1d_data_array_dimension(data: xr.DataArray) -> xr.DataArray:
