71b8fc7a2d1957f621f6357a186646465b4ee466
Fabian Emmerich <fabian.emmerich@4-cast.de>
2022-10-14 16:10:35 +0200

    Add test

--- src/tests/unit/utils/test_standardization.py
+++ src/tests/unit/utils/test_standardization.py
@@ -1,5 +1,6 @@
 import a6.utils.standardization as standardization
 import numpy as np
+import xarray as xr
 
 
 def test_standarize():
@@ -11,3 +12,28 @@ def test_standarize():
     result = standardization.standardize(data)
 
     np.testing.assert_equal(result, expected)
+
+
+def test_standarize_features():
+    data = xr.DataArray(
+        [
+            [1.0, 4.0],
+            [2.0, 5.0],
+            [3.0, 6.0],
+        ]
+    )
+    mean_1 = (1 + 2 + 3) / 3
+    mean_2 = (4 + 5 + 6) / 3
+    std_1 = np.sqrt(
+        ((1 - mean_1) ** 2 + (2 - mean_1) ** 2 + (3 - mean_1) ** 2) / 3
+    )
+    std_2 = np.sqrt(
+        ((4 - mean_2) ** 2 + (5 - mean_2) ** 2 + (6 - mean_2) ** 2) / 3
+    )
+    expected_1 = np.array([(1 - mean_1), (2 - mean_1), (3 - mean_1)]) / std_1
+    expected_2 = np.array([(4 - mean_2), (5 - mean_2), (6 - mean_2)]) / std_2
+    expected = xr.DataArray(list(zip(expected_1, expected_2)))
+
+    result = standardization.standardize_features(data)
+
+    xr.testing.assert_equal(result, expected)
