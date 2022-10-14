e159452a389b860f37504d397c438193a1d8ba6f
Fabian Emmerich <fabian.emmerich@4-cast.de>
2022-10-14 21:29:14 +0200

    Fix test

--- src/tests/unit/plotting/modes/test_geopotential_plotting.py
+++ src/tests/unit/plotting/modes/test_geopotential_plotting.py
@@ -1,21 +1,19 @@
-import a6.features.methods as methods
 import a6.plotting.modes as modes
 import pytest
 import xarray as xr
 
 
-@pytest.fixture(scope="session")
-def geopotential_height(pl_ds) -> xr.DataArray:
-    return methods.calculate_geopotential_height(pl_ds["z"])
-
-
 @pytest.mark.parametrize("temperature", [None, "ml_ds"])
-def test_plot_geopotential_height_contours(
-    request, geopotential_height, temperature
-):
+def test_plot_geopotential_height_contours(request, temperature):
     if temperature is not None:
-        temperature = request.getfixturevalue(temperature)
+        temperature = request.getfixturevalue(temperature)["u"].isel(time=0)
 
+    geopotential_height = xr.DataArray(
+        [
+            [500, 510],
+            [505, 515],
+        ],
+    )
     modes.plot_geopotential_height_contours(
         geopotential_height, temperature=temperature
     )
