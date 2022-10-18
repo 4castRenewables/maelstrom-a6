7d87e1ef2e47b2516d4279e86796ce102cd8d033
Fabian Emmerich <fabian.emmerich@4-cast.de>
2022-10-18 15:44:11 +0200

    Add xarray type

--- src/a6/types.py
+++ src/a6/types.py
@@ -4,4 +4,5 @@ import numpy as np
 import xarray as xr
 
 Data = t.Union[np.ndarray, xr.DataArray]
+XarrayData = t.Union[xr.DataArray, xr.Dataset]
 DataND = t.Union[np.ndarray, xr.DataArray, xr.Dataset]
