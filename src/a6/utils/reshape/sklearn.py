544c91aa80dfeea73a0608e8c78f28aec79502f7
Fabian Emmerich <fabian.emmerich@4-cast.de>
2022-10-20 14:50:14 +0200

    Use only xarray types

--- src/a6/utils/reshape/sklearn.py
+++ src/a6/utils/reshape/sklearn.py
@@ -3,14 +3,14 @@ import numpy as np
 import xarray as xr
 
 
-def transpose(*data: types.Data) -> np.ndarray:
+def transpose(*data: types.XarrayData) -> np.ndarray:
     """Transpose a given 1D dataset according to the sklearn interface."""
     if len(data) == 1:
         return _reshape(data[0])
     return np.array(list(zip(data)))
 
 
-def _reshape(data: types.Data) -> np.ndarray:
+def _reshape(data: types.XarrayData) -> np.ndarray:
     if isinstance(data, xr.DataArray):
         return data.values.reshape(-1, 1)
     return data.reshape(-1, 1)
