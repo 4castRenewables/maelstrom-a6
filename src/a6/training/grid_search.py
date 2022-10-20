07ab490a68de15df91898e903b3a24d60a47cfa0
Fabian Emmerich <fabian.emmerich@4-cast.de>
2022-10-20 14:52:44 +0200

    Move to training package

---
+++ src/a6/training/grid_search.py
@@ -0,0 +1,26 @@
+import typing as t
+
+import a6.types as types
+import a6.utils as utils
+import sklearn.model_selection as model_selection
+
+
+def perform_grid_search(
+    model: types.Model,
+    parameters: dict[str, list],
+    X: types.XarrayData,
+    y: types.XarrayData,
+    cv: types.Scorers,
+    scorers: types.Scorers,
+    refit: t.Optional[str] = None,
+) -> model_selection.GridSearchCV:
+    """Perform a parameter grid search on a dataset."""
+    gs = model_selection.GridSearchCV(
+        estimator=model,
+        param_grid=parameters,
+        scoring=scorers,
+        cv=cv,
+        refit=refit,
+        n_jobs=utils.get_cpu_count(),
+    )
+    return gs.fit(X=utils.transpose(X), y=utils.transpose(y))
---
+++ src/a6/training/train.py
@@ -0,0 +1,14 @@
+import a6.types as types
+import a6.utils as utils
+import sklearn.base
+import xarray as xr
+
+
+def fit(
+    model: types.Model,
+    X: xr.DataArray,
+    y: xr.DataArray,
+    scorers: types.Scorers,
+) -> sklearn.base.RegressorMixin:
+    """Train a given model."""
+    return model.fit(X=utils.transpose(X), y=utils.transpose(y))
