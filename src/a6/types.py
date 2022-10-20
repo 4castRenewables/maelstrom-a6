51348d9a4ed8e0af7125cc2b0ce95b7384ddca70
Fabian Emmerich <fabian.emmerich@4-cast.de>
2022-10-20 14:51:04 +0200

    Add metric types

--- src/a6/types.py
+++ src/a6/types.py
@@ -6,3 +6,23 @@ import xarray as xr
 Data = t.Union[np.ndarray, xr.DataArray]
 XarrayData = t.Union[xr.DataArray, xr.Dataset]
 DataND = t.Union[np.ndarray, xr.DataArray, xr.Dataset]
+
+
+class MetricEstimator(t.Protocol):
+    """Estimates a metric given a ground truth and a prediction."""
+
+    def __call__(self, y_true: Data, y_pred: Data) -> float:
+        """Calculate a certain metric."""
+
+
+class Model(t.Protocol):
+    """Estimates a metric given a ground truth and a prediction."""
+
+    def fit(self, X: np.ndarray, y: np.ndarray) -> "Model":
+        """Fit the model to the given data."""
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """Make a prediction."""
+
+
+Scorers = dict[str, MetricEstimator]
