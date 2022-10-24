b7e9e1959ad9a577b1b685e4a41998c58588b6ec
Fabian Emmerich <fabian.emmerich@4-cast.de>
2022-10-24 17:57:35 +0200

    Allow passing of config to adapt grid search

--- src/tests/unit/studies/test_grid_search.py
+++ src/tests/unit/studies/test_grid_search.py
@@ -1,8 +1,17 @@
 import a6.studies.grid_search as forecast
+import sklearn.ensemble as ensemble
 import xarray as xr
 
 
 def test_perform_forecast_model_grid_search(pl_ds):
+    model = ensemble.GradientBoostingRegressor
+    parameters = {
+        "learning_rate": [0.1],
+        "n_estimators": [50],
+        "min_samples_split": [2],
+        "min_samples_leaf": [1],
+        "max_depth": [3],
+    }
     weather = pl_ds.sel(level=500)
     turbine = xr.Dataset(
         data_vars={"production": [i for i in range(49)]},
@@ -15,6 +24,8 @@ def test_perform_forecast_model_grid_search(pl_ds):
     )
 
     forecast.perform_forecast_model_grid_search(
+        model=model,
+        parameters=parameters,
         weather=weather,
         turbine=turbine,
         log_to_mantik=False,
