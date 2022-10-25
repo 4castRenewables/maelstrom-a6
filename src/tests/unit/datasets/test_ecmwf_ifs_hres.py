aa9157813b17b8d8ceb21a679ba6d5d8c1054c50
Fabian Emmerich <fabian.emmerich@4-cast.de>
2022-10-25 15:25:27 +0200

    Allow reading folders with patterns

--- src/tests/unit/datasets/test_ecmwf_ifs_hres.py
+++ src/tests/unit/datasets/test_ecmwf_ifs_hres.py
@@ -12,32 +12,44 @@ DATA_DIR = FILE_PATH / "../../data"
 
 class TestEcmwfIfsHresDataset:
     @pytest.mark.parametrize(
-        ("paths", "overlapping", "expected"),
+        ("path", "pattern", "slice_time_dimension", "expected"),
         [
-            ([], False, ValueError()),
             (
-                [DATA_DIR / "ml_20190101_00.nc"],
+                DATA_DIR / "ml_20190101_00.nc",
+                "should be ignored",
                 False,
                 pd.date_range(
                     "2019-01-01T00:00", "2019-01-03T00:00", freq="1h"
                 ),
             ),
+            # Test case: expected ml_20190101_00.nc and ml_20190101_12.nc
+            # to be read.
             (
-                [
-                    DATA_DIR / "ml_20190101_00.nc",
-                    DATA_DIR / "ml_20190101_12.nc",
-                ],
+                DATA_DIR,
+                "ml_20190101_*.nc",
                 True,
                 pd.date_range(
                     "2019-01-01T00:00", "2019-01-01T23:00", freq="1h"
                 ),
             ),
+            # Test case: expected ml_20190101_12.nc to be read.
+            (
+                DATA_DIR,
+                "ml_20190101_12.nc",
+                True,
+                pd.date_range(
+                    "2019-01-01T12:00", "2019-01-01T23:00", freq="1h"
+                ),
+            ),
         ],
     )
-    def test_as_xarray(self, paths, overlapping, expected):
+    def test_as_xarray(self, path, pattern, slice_time_dimension, expected):
         with testing.expect_raise_if_exception(expected):
             dataset = ecmwf_ifs_hres.EcmwfIfsHres(
-                paths, overlapping=overlapping, parallel_loading=False
+                path,
+                pattern=pattern,
+                slice_time_dimension=slice_time_dimension,
+                parallel_loading=False,
             )
             result = dataset.as_xarray()
             result_dates = result["time"].values.astype("datetime64[m]")
