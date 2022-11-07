61014c3458ac1425f8f933484c52cff5da05c0ac
Fabian Emmerich <fabian.emmerich@4-cast.de>
2022-11-07 17:05:31 +0100

    Fix plotting of LSWR durations

--- src/tests/unit/plotting/modes/test_statistics_plotting.py
+++ src/tests/unit/plotting/modes/test_statistics_plotting.py
@@ -1,5 +1,11 @@
-import a6.plotting.modes as modes
+import a6.plotting.modes.statistics as statistics
 
 
 def test_plot_modes_durations(mode_appearances):
-    modes.plot_modes_durations(mode_appearances)
+    statistics.plot_modes_durations(mode_appearances)
+
+
+def test_calculate_mean_durations_and_standard_deviations(mode_appearances):
+    statistics._calculate_mean_durations_and_standard_deviations(
+        mode_appearances
+    )
