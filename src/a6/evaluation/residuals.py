5f5a478e599b266ff1f699711ad6d8799583db4a
Fabian Emmerich <fabian.emmerich@4-cast.de>
2022-11-07 15:04:51 +0100

    Make ssr calculation a Functional

--- src/a6/evaluation/residuals.py
+++ src/a6/evaluation/residuals.py
@@ -1,15 +1,18 @@
+import a6.utils as utils
 import numpy as np
 import xarray as xr
 
 
+@utils.make_functional
 def calculate_ssr(left: xr.DataArray, right: xr.DataArray) -> float:
     """Calculate the sum of squared residuals between two datasets."""
     return float(((left - right) ** 2).sum().values)
 
 
+@utils.make_functional
 def calculate_normalized_root_ssr(
     left: xr.DataArray, right: xr.DataArray
 ) -> float:
     """Calculate the normalized root SSR between two datasets."""
-    ssr = calculate_ssr(left, right)
+    ssr = calculate_ssr(left, right, non_functional=True)
     return np.sqrt(ssr) / left.max()
--- src/tests/unit/evaluate/test_residuals.py
+++ src/tests/unit/evaluate/test_residuals.py
@@ -1,16 +1,37 @@
 import a6.evaluation.residuals as residuals
 import numpy as np
+import pandas as pd
 import pytest
 import xarray as xr
+
+TIME = pd.date_range("2000-01-01", "2000-01-02", freq="1d")
+LATS = [1.0, 0.0]
+LONS = [0.0, 1.0]
+
+COORDS = {
+    "time": TIME,
+    "latitude": LATS,
+    "longitude": LONS,
+}
+
+DIMS = ["time", "latitude", "longitude"]
 
 
 @pytest.fixture()
 def left() -> xr.DataArray:
     return xr.DataArray(
         [
-            [1.0, 2.0],
-            [3.0, 4.0],
-        ]
+            [
+                [1.0, 2.0],
+                [3.0, 4.0],
+            ],
+            [
+                [5.0, 6.0],
+                [7.0, 8.0],
+            ],
+        ],
+        coords=COORDS,
+        dims=DIMS,
     )
 
 
@@ -18,9 +39,17 @@ def left() -> xr.DataArray:
 def right() -> xr.DataArray:
     return xr.DataArray(
         [
-            [2.0, 4.0],
-            [6.0, 8.0],
-        ]
+            [
+                [2.0, 4.0],
+                [6.0, 8.0],
+            ],
+            [
+                [10.0, 12.0],
+                [14.0, 16.0],
+            ],
+        ],
+        coords=COORDS,
+        dims=DIMS,
     )
 
 
@@ -32,19 +61,25 @@ def expected_ssr() -> float:
             (2.0 - 4.0) ** 2,
             (3.0 - 6.0) ** 2,
             (4.0 - 8.0) ** 2,
+            (5.0 - 10.0) ** 2,
+            (6.0 - 12.0) ** 2,
+            (7.0 - 14.0) ** 2,
+            (8.0 - 16.0) ** 2,
         ]
     )
 
 
 def test_calculate_ssr(left, right, expected_ssr):
-    result = residuals.calculate_ssr(left, right)
+    result = residuals.calculate_ssr(left, right, non_functional=True)
 
     assert result == expected_ssr
 
 
 def test_calculate_normalized_root_ssr(left, right, expected_ssr):
-    expected = np.sqrt(expected_ssr) / 4.0
+    expected = np.sqrt(expected_ssr) / 8.0
 
-    result = residuals.calculate_normalized_root_ssr(left, right)
+    result = residuals.calculate_normalized_root_ssr(
+        left, right, non_functional=True
+    )
 
     assert result == expected
