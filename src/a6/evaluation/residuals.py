5ca098d64b564f8c94b9102459449a91ccea2fff
Fabian Emmerich <fabian.emmerich@4-cast.de>
2022-12-06 12:25:55 +0100

    Add note for normalization

--- src/a6/evaluation/residuals.py
+++ src/a6/evaluation/residuals.py
@@ -14,6 +14,10 @@ def calculate_ssr(y_pred: xr.DataArray, y_true: xr.DataArray) -> float:
 def calculate_normalized_root_ssr(
     y_pred: xr.DataArray, y_true: xr.DataArray
 ) -> float:
-    """Calculate the normalized root SSR between two datasets."""
+    """Calculate the normalized root SSR between two datasets.
+
+    The SSR is normalized to the maximum value of `y_true`.
+
+    """
     ssr = calculate_ssr(y_pred=y_pred, y_true=y_true, non_functional=True)
     return np.sqrt(ssr) / y_true.max()
