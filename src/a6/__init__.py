34b2583dec385e4a3d4d71a6dfd5856c57033f04
Fabian Emmerich <fabian.emmerich@4-cast.de>
2023-09-02 12:20:08 +0200

    Delete parallel module (dask, ipyparallel)

--- src/a6/__init__.py
+++ src/a6/__init__.py
@@ -4,7 +4,6 @@ import a6.datasets
 import a6.evaluation
 import a6.features
 import a6.modes
-import a6.parallel
 import a6.plotting
 import a6.studies
 import a6.testing
--- src/a6/parallel/types.py
+++
@@ -1,5 +0,0 @@
-from collections.abc import Callable
-from collections.abc import Iterable
-
-Method = Callable
-Arguments = Iterable[Iterable]
