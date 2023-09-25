befd7b43423f8125070bb44c41d89778c65ec0b4
Fabian Emmerich <fabian.emmerich@4-cast.de>
2023-09-25 16:19:18 +0200

    Attempt to create files on all ra nks

--- src/a6/dcv2/_initialization.py
+++ src/a6/dcv2/_initialization.py
@@ -26,28 +26,22 @@ def initialize_logging(args, columns) -> tuple[logging.Logger, logs.Stats]:
     args.dump_plots = args.dump_results / "plots"
     args.dump_tensors = args.dump_results / "tensors"
 
+    for path in [
+        args.dump_checkpoints,
+        args.dump_results,
+        args.dump_plots,
+        args.dump_tensors,
+    ]:
+        if not path.exists():
+            path.mkdir(parents=True, exist_ok=True)
+
     if _is_primary_device(args):
         # dump parameters
-        if not args.dump_path.exists():
-            args.dump_path.mkdir(parents=True, exist_ok=True)
-
         with open(args.dump_path / "args.yaml", "w") as f:
             data = {
                 key: _path_to_string(value) for key, value in vars(args).items()
             }
             yaml.safe_dump(data, stream=f, indent=2, default_flow_style=False)
-
-        # create repo to store checkpoints
-        if not args.global_rank and not args.dump_checkpoints.is_dir():
-            args.dump_checkpoints.mkdir(parents=True, exist_ok=True)
-
-        # create repo to store plots
-        if not args.global_rank and not args.dump_plots.is_dir():
-            args.dump_plots.mkdir(parents=True, exist_ok=True)
-
-        # create repo to store tensors
-        if not args.global_rank and not args.dump_tensors.is_dir():
-            args.dump_tensors.mkdir(parents=True, exist_ok=True)
 
     # create a panda object to log loss and acc
     training_stats = logs.Stats(
@@ -56,7 +50,7 @@ def initialize_logging(args, columns) -> tuple[logging.Logger, logs.Stats]:
 
     # create a logger
     logger_ = logs.create_logger(
-        args.dump_path / f"train-{args.global_rank}.csv", args=args
+        args.dump_path / f"train-{args.global_rank}.log", args=args
     )
 
     if _is_primary_device(args):
