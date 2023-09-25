9d0ce93780d391ef68abb828e903bbe25e4bf9d4
Fabian Emmerich <fabian.emmerich@4-cast.de>
2023-09-25 16:21:35 +0200

    Add tests

---
+++ src/tests/unit/plotting/test_embeddings.py
@@ -0,0 +1,41 @@
+import torch
+
+import a6.plotting.embeddings as _embeddings
+
+
+def test_plot_embedding(tmp_path):
+    # Embeddings for 2 crops, 2 samples, and embedding dims of size 4
+    embeddings = torch.Tensor(
+        [
+            [
+                [1, 2, 3, 4],
+                [5, 6, 7, 8],
+            ],
+            [
+                [1, 2, 3, 4],
+                [5, 6, 7, 8],
+            ],
+        ]
+    )
+
+    # Assignments for 2 samples
+    assignments = torch.Tensor(
+        [1, 2],
+    )
+
+    centroids = torch.Tensor(
+        [0, 1],
+    ).int()
+
+    expected = tmp_path / "test-crops-0.pdf"
+
+    _embeddings.plot_embeddings_using_tsne(
+        embeddings=embeddings,
+        j=0,
+        assignments=assignments,
+        centroids=centroids,
+        name="test",
+        output_dir=tmp_path.as_posix(),
+    )
+
+    assert expected.exists()
