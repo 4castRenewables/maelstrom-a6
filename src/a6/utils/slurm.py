9fbe18ae752cf02205d1fca93d657bfbf9ae3a9e
Fabian Emmerich <fabian.emmerich@4-cast.de>
2023-09-26 15:35:55 +0200

    Remove ununsed slurm methods

--- src/a6/utils/slurm.py
+++ src/a6/utils/slurm.py
@@ -1,8 +1,6 @@
 import logging
 import os
 
-import torch
-
 logger = logging.getLogger(__name__)
 
 
@@ -16,29 +14,6 @@ def get_number_of_nodes() -> int:
 
 def get_node_id():
     return int(os.getenv("SLURM_NODEID", 0))
-
-
-def get_global_rank(args) -> int:
-    return get_node_id() * get_gpus_per_node() + args.local_rank
-
-
-def get_world_size() -> int:
-    n_nodes = get_number_of_nodes()
-    gpus_per_node = get_gpus_per_node()
-    size = n_nodes * gpus_per_node if gpus_per_node > 0 else n_nodes
-    logger.info("Detected world size of %s devices", size)
-    return size
-
-
-def get_gpus_per_node() -> int:
-    if torch.cuda.is_available():
-        gpus_per_node = torch.cuda.device_count()
-    elif visible_devices := os.getenv("CUDA_VISIBLE_DEVICES", ""):
-        gpus_per_node = len(visible_devices.split(","))
-    else:
-        gpus_per_node = 0
-    logger.info("Found %s GPUs available per node", gpus_per_node)
-    return gpus_per_node
 
 
 def get_slurm_job_id() -> str:
@@ -63,7 +38,6 @@ def get_slurm_env_vars() -> dict[str, str]:
         "SLURM_JOB_GPUS": os.getenv("SLURM_JOB_GPUS"),
         "SLURM_JOB_STDOUT": os.getenv("SLURM_JOB_STDOUT"),
         "SLURM_JOB_STDERR": os.getenv("SLURM_JOB_STDERR"),
-        "GPUS_PER_NODE": str(get_gpus_per_node()),
     }
 
 
--- src/tests/unit/utils/test_slurm.py
+++ src/tests/unit/utils/test_slurm.py
@@ -1,36 +1,9 @@
-import pytest
-
 import a6.testing as testing
 import a6.utils.slurm as slurm
 
 
 def test_get_node_id():
     with testing.env.env_vars_set({"SLURM_NODEID": "2"}):
-        result = slurm.get_node_id(1)
+        result = slurm.get_node_id()
 
     assert result == 2
-
-
-def test_get_node_id_unset():
-    result = slurm.get_node_id(1)
-
-    assert result == 1
-
-
-@pytest.mark.parametrize(
-    ("n_nodes", "visible_devices", "expected"),
-    [
-        (1, None, 1),
-        (1, "", 1),
-        (1, "1", 1),
-        (1, "1,2", 2),
-        (2, "1,2", 4),
-    ],
-)
-def test_get_world_size(n_nodes, visible_devices, expected):
-    with testing.env.env_vars_set(
-        {"SLURM_NNODES": str(n_nodes), "CUDA_VISIBLE_DEVICES": visible_devices}
-    ):
-        result = slurm.get_world_size()
-
-    assert result == expected
