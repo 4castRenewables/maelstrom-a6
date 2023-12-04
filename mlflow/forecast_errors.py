4621d32a5bd052a5c9c8cf9d96f7da6a5d6ba26a
Fabian Emmerich <fabian.emmerich@4-cast.de>
2023-12-04 15:05:51 +0100

    Log data for calculate_wind_speed

--- mlflow/forecast_errors.py
+++ mlflow/forecast_errors.py
@@ -1,6 +1,8 @@
+import logging
+
 import a6
 
 
 if __name__ == "__main__":
-    a6.utils.logging.log_to_stdout()
+    a6.utils.logging.log_to_stdout(level=logging.DEBUG)
     a6.evaluation.forecast.simulate_forecast_errors()
